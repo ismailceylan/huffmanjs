653a9654f1ca0105d6f3b4013d9d0919811a2675
Ismail Ceylan <ismaillceylan@gmail.com>
2024-09-01 15:42:31 +0300

    build: compile library

--- dist/huffmanjs.js
+++ dist/huffmanjs.js
@@ -1,144 +1,146 @@
-function g(n) {
-  const e = {};
+function g(e) {
+  const t = {};
   let r = "";
-  for (const t of n)
-    r += t.charCodeAt(0).toString(2).padStart(8, "0");
-  for (let t = 0; t < r.length; t += 32) {
-    const o = r.slice(t, t + 16), i = r.slice(t + 16, t + 32), c = parseInt(o, 2), l = i.replace(/^0*(.*)/, "$1");
-    e[l] = c;
+  for (const n of e)
+    r += n.charCodeAt(0).toString(2).padStart(8, "0");
+  for (let n = 0; n < r.length; n += 32) {
+    const o = r.slice(n, n + 16), c = r.slice(n + 16, n + 32), s = parseInt(o, 2), l = c.replace(/^0*(.*)/, "$1");
+    t[l] = s;
   }
-  return e;
+  return t;
 }
-function p(n) {
-  if (/[0-7]/.test(n[0]))
-    throw new Error("Invalid padding data!");
-  return [
-    parseInt(
-      n[0].charCodeAt(0)
-    ),
-    n.slice(1)
-  ];
+function p(e) {
+  let t = e.split("").map(
+    (r) => r.charCodeAt(0)
+  );
+  return t[0] > 0 && (t[0] *= 255, t[0]++), t.reduce((r, n) => r + n, 0);
 }
-function m(n) {
-  let e = "";
-  for (const o in n) {
-    const i = n[o], c = 16 - i.length % 16, l = i.padStart(i.length + c, "0"), h = parseInt(o).toString(2).padStart(16, "0");
-    e += h + l;
+function b(e) {
+  let t = "";
+  for (const o in e) {
+    const c = e[o], s = 16 - c.length % 16, l = c.padStart(c.length + s, "0"), f = parseInt(o).toString(2).padStart(16, "0");
+    t += f + l;
   }
-  const r = a(e)[0], t = a(
+  const r = a(t)[0], n = a(
     r.length.toString(2).padStart(16, "0")
   )[0];
-  return [r, t];
+  return [r, n];
 }
-function w(n) {
-  const e = {};
-  r(n[0]);
-  function r(t, o = "1") {
-    t !== null && (t.char !== null && (e[t.char] = o), r(t.left, o + "1"), r(t.right, o + "0"));
+function m(e) {
+  const t = {};
+  r(e[0]);
+  function r(n, o = "1") {
+    n !== null && (n.char !== null && (t[n.char] = o), r(n.left, o + "1"), r(n.right, o + "0"));
   }
-  return e;
+  return t;
 }
-function b(n) {
-  const e = {};
-  for (let r = 0; r < n.length; r++) {
-    const t = n[r];
-    e[t] ? e[t]++ : e[t] = 1;
+function y(e) {
+  const t = {};
+  for (let r = 0; r < e.length; r++) {
+    const n = e[r];
+    t[n] ? t[n]++ : t[n] = 1;
   }
-  return e;
+  return t;
 }
-function S(n, e) {
+function S(e, t) {
   let r = "";
-  for (const t of n)
-    r += e[t];
+  for (const n of e)
+    r += t[n];
   return r;
 }
-const s = "Ñur!", f = "ÿÿ";
-function C([n, e], r) {
-  const [t, o] = m(r);
+const i = "íce9";
+function C([e, t], r) {
+  const [n, o] = b(r), c = String.fromCharCode(t), s = [
+    i,
+    c,
+    o,
+    n,
+    e
+  ], l = [
+    i,
+    c,
+    e
+  ];
   return [
-    // magic bytes
-    s,
-    // zero padding as character
-    String.fromCharCode(e),
-    t,
-    // meta ending declaration
-    f,
-    // encoded data
+    s.join(""),
+    l.join(""),
     n
-  ].join("");
-}
-function y(n) {
-  return q(k(n));
-}
-function k(n) {
-  const e = {};
-  if (n.slice(0, s.length) !== s)
-    throw new Error("Unknown file format!");
-  const r = n.indexOf(f);
-  if (r === -1)
-    throw new Error("Meta ending not found!");
-  let t = n.slice(s.length, r);
-  return [e.padEnd, t] = p(t), e.bitmap = g(t), e.raw = n.slice(r + f.length), e;
-}
-function B(n) {
-  n = n.split("").map(
-    (c) => c.charCodeAt(0)
-  );
-  const e = b(n), r = E(e), t = w(r), o = S(n, t), i = a(o);
-  return [
-    C(i, t),
-    t
   ];
 }
-function E(n) {
-  const e = Object.keys(n).map((r) => new d(r, n[r])).sort(u);
-  for (; e.length > 1; ) {
-    const r = e.shift(), t = e.shift(), o = new d(null, r.freq + t.freq);
-    o.left = r, o.right = t, e.push(o), e.sort(u);
-  }
-  return e;
+function B(e, t) {
+  return q(k(e, t));
 }
-function a(n) {
-  const e = [];
+function k(e, t) {
+  if (e.slice(0, i.length) !== i)
+    throw new Error("Unknown file format!");
+  const r = {
+    padEnd: e[i.length].charCodeAt(0)
+  }, n = t ? t.length : p(
+    e[i.length + 1] + e[i.length + 2]
+  ), o = t || e.slice(i.length + 3, i.length + 3 + n);
+  return r.bitmap = g(o), r.raw = t ? e.slice(i.length + 1) : e.slice(i.length + 3 + n), r;
+}
+function L(e) {
+  e = e.split("").map(
+    (d) => d.charCodeAt(0)
+  );
+  const t = y(e), r = w(t), n = m(r), o = S(e, n), c = a(o), [s, l, f] = C(c, n);
+  return [
+    s,
+    n,
+    l,
+    f
+  ];
+}
+function w(e) {
+  const t = Object.keys(e).map((r) => new h(r, e[r])).sort(u);
+  for (; t.length > 1; ) {
+    const r = t.shift(), n = t.shift(), o = new h(null, r.freq + n.freq);
+    o.left = r, o.right = n, t.push(o), t.sort(u);
+  }
+  return t;
+}
+function a(e) {
+  const t = [];
   let r = 0;
-  for (let t = 0; t < n.length; t += 8) {
-    let o = n.slice(t, t + 8);
+  for (let n = 0; n < e.length; n += 8) {
+    let o = e.slice(n, n + 8);
     o.length < 8 && (r = 8 - o.length, o = o.padEnd(8, "0"));
-    const i = parseInt(o, 2), c = String.fromCharCode(i);
-    e.push(c);
+    const c = parseInt(o, 2), s = String.fromCharCode(c);
+    t.push(s);
   }
-  return [e.join(""), r];
+  return [t.join(""), r];
 }
-function q(n) {
-  let e = "";
-  for (const o of n.raw)
-    e += o.charCodeAt(0).toString(2).padStart(8, "0");
-  n.padEnd > 0 && (e = e.slice(0, -n.padEnd));
-  let r = [], t = "";
-  for (const o of e) {
-    t += o;
-    const i = n.bitmap[t];
-    i !== void 0 && (r.push(String.fromCharCode(i)), t = "");
+function q(e) {
+  let t = "";
+  for (const o of e.raw)
+    t += o.charCodeAt(0).toString(2).padStart(8, "0");
+  e.padEnd > 0 && (t = t.slice(0, -e.padEnd));
+  let r = [], n = "";
+  for (const o of t) {
+    n += o;
+    const c = e.bitmap[n];
+    c !== void 0 && (r.push(String.fromCharCode(c)), n = "");
   }
   return r.join("");
 }
-function u(n, e) {
-  return n.freq - e.freq;
+function u(e, t) {
+  return e.freq - t.freq;
 }
-class d {
+class h {
   /**
    * Creates a new instance.
    *
    * @param {string} char - The character represented by the node.
    * @param {number} freq - The frequency of the character.
    */
-  constructor(e, r) {
-    this.char = e, this.freq = r, this.left = null, this.right = null;
+  constructor(t, r) {
+    this.char = t, this.freq = r, this.left = null, this.right = null;
   }
 }
 export {
-  B as compress,
-  y as decompress,
-  m as serializeHuffmanCode,
+  L as compress,
+  B as decompress,
+  b as serializeHuffmanCode,
   g as unserializeHuffmanCode
 };
